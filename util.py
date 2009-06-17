a78d4211e2fcd3784848bb5fd4c7b067937adee4
Manuel Cerón <ceronman@gmail.com>
2009-06-16 23:00:24 -0500

    Added util module with utility functions.
    Added a handle_exception decorator.

---
+++ util.py
@@ -0,0 +1,23 @@
+import functools
+
+def handle_exception(exception_class, handler):
+    if not isinstance(exception_class, Exception):
+        exception_class = Exception
+    if not hasattr(handler, '__call__'):
+        def new_handler(e):
+            raise e
+
+        handler = new_handler
+
+    def decorator(function):
+        @functools.wraps(function)
+        def wrapper(*args, **kwargs):
+            try:
+                return function(*args, **kwargs)
+            except exception_class as e:
+                handler(e)
+
+        return wrapper
+
+    return decorator
+
