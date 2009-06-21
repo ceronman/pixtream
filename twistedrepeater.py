ff929d8c7a210ea474bf39d60402e1c13e0cee4c
Manuel Cerón <ceronman@gmail.com>
2009-06-21 01:58:52 -0500

    Added the option for None seconds

--- twistedrepeater.py
+++ twistedrepeater.py
@@ -4,14 +4,15 @@ class TwistedRepeaterException(Exception):
     pass
 
 class TwistedRepeater(object):
-    def __init__(self, function, seconds):
+    def __init__(self, function, seconds=None):
         self.function = function
         self.seconds = seconds
         self.call = None
 
     def repeater(self, *args, **kwargs):
-        self.call = reactor.callLater(self.seconds, self.repeater,
-                                      *args, **kwargs)
+        if self.seconds is not None:
+            self.call = reactor.callLater(self.seconds, self.repeater,
+                                          *args, **kwargs)
         self.function(*args, **kwargs)
 
     def start(self, *args, **kwargs):
