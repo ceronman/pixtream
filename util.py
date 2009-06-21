e97f92d08c9992e6cc8025070a3b28bad5f7044a
Manuel Cerón <ceronman@gmail.com>
2009-06-21 02:29:36 -0500

    Removed all TwistedTimer class

--- util.py
+++ util.py
@@ -1,5 +1,4 @@
 import functools
-from twisted.internet import reactor
 
 def handle_exception(exception_class, handler):
     if not isinstance(exception_class, Exception):
@@ -21,24 +20,4 @@ def handle_exception(exception_class, handler):
         return wrapper
 
     return decorator
-
-class TwistedTimerException(Exception):
-    pass
-
-class TwistedTimer(object):
-    def __init__(self, function, lapse):
-        self.function = function
-        self.lapse = lapse
-        self.calling = False
-
-    def repeater(self, *args, **kwargs)
-        self.function(*args, **kwargs)
-        self.call = reactor.callLater(selflapse, repeater)
-
-    def call(self, *args, **kwargs):
-        if self.calling:
-            raise TwistedTimerException('Timer already called')
-        self.repeater(*args, **kwargs)
-        self.calling = True
-
 
