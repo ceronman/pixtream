81777a2af7d62b7678f40bbbf21294de20e77e7a
ceronman <devnull@localhost>
2009-06-23 00:42:03 -0500

    generate_peer_id is now a separate function

--- pixtream/peer/peerservice.py
+++ pixtream/peer/peerservice.py
@@ -2,20 +2,25 @@ from uuid import uuid4
 
 from twisted.application.service import Service
 
+from pixtream.peer.trackermanager import TrackerManager
+
+def generate_peer_id():
+    id = uuid4().hex
+    id = id[:16]
+    return 'PXTM' +  id
+
 class PeerService(Service):
-    def __init__(self, tracker_url, port, ip):
+    def __init__(self, ip, port, tracker_url):
         self.peers = []
-        self.tracker_url = tracker_url
         self.port = port
         self.ip = ip
-        self.peer_id = self.generate_peer_id()
+        self.peer_id = generate_peer_id()
 
-        self.i = 0
-        self.test_timer.start(self)
+        self._tracker_manager = TrackerManager(self, tracker_url)
 
-    def generate_peer_id(self):
-        id = uuid4().hex
-        id = id[:16]
-        return 'PXTM' +  id
+    def connect_to_tracker(self):
+        self._tracker_manager.connect_to_tracker()
+
+
 
 
