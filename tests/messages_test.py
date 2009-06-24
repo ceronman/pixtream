f0bf418ba931991954ca4c56d695991c0feab67d
Manuel Cerón <ceronman@gmail.com>
2009-06-23 23:18:18 -0500

    Updated test for messages module

--- tests/messages_test.py
+++ tests/messages_test.py
@@ -1,16 +1,30 @@
 import unittest
-import pixtream.peer.messages as messages
 import random
 import string
+
+import pixtream.peer.messages as msg
 
 class HandShakeMessageTest(unittest.TestCase):
     def test_iomessage(self):
         peer_id = ''.join(random.choice(string.letters) for _ in range(20))
 
-        output = messages.handshake_encode(peer_id)
-        input = messages.handshake_decode(output)
+        handshake = msg.HandshakeMessage(peer_id)
 
-        self.assertEqual(peer_id, input[-1])
+        bytes = handshake.encode()
+        new_handshake = msg.Message.decode(bytes)
+
+        self.assert_(new_handshake.validate())
+        self.assertEqual(new_handshake.peer_id, peer_id)
+
+    def test_prefix(self):
+        peer_id = ''.join(random.choice(string.letters) for _ in range(20))
+
+        handshake = msg.HandshakeMessage(peer_id)
+
+        bytes = handshake.encode()
+        prefixed_bytes = handshake.prefix_encode()
+
+        self.assertEqual(prefixed_bytes[:4], '\x00\x00\x00.')
 
 if __name__ == '__main__':
     unittest.main()
