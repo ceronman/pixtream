5d13975edfc89d2ac7bd49d4d7b70d93735312cf
Manuel Cerón <ceronman@gmail.com>
2009-07-02 01:06:10 -0500

    PixtreamProtocol renamed and splited in IncomingProtocol and OutgoingProtocol ConnectionManager now uses two different lists for outgoing and incoming connections

--- pixtream/peer/connectionmanager.py
+++ pixtream/peer/connectionmanager.py
@@ -3,66 +3,100 @@ import logging
 from twisted.internet.protocol import ServerFactory, ClientFactory
 from twisted.internet import reactor
 
-from pixtream.peer.protocol import PixtreamProtocol
+from pixtream.peer.protocol import IncomingProtocol, OutgoingProtocol
 from pixtream.util.twistedrepeater import TwistedRepeater
+from pixtream.peer.peerdatabase import Peer
 
 CHECK_INTERVAL = 5
 
 class ConnectionManager(object):
 
     def __init__(self, peer_service):
+        self.incoming_connections = {}
+        self.outgoing_connections = {}
+
         self._peer_service = peer_service
-        self.active_connections = {}
+
         self._checker_repeater = TwistedRepeater(self.check_connections,
                                                  CHECK_INTERVAL)
         self._checker_repeater.start_later()
 
-    def configure_factory(self, factory):
-        factory.protocol = PixtreamProtocol
-        factory.peer_id = self._peer_service.peer_id
-        factory.add_connection = self.add_connection
-        factory.remove_connection = self.remove_connection
-        factory.allow_connection = self.allow_connection
+    @property
+    def contacted_peers(self):
+        return set(self.incoming_connections.keys() +
+                   self.outgoing_connections.keys())
 
     def create_server_factory(self):
         factory = ServerFactory()
-        self.configure_factory(factory)
-        factory.target_id = None
+        factory.protocol = IncomingProtocol
+        factory.peer_id = self._peer_service.peer_id
+        factory.add_incoming_connection = self.add_incoming_connection
+        factory.remove_incoming_connection = self.remove_incoming_connection
+        factory.allow_connection = self.allow_connection
 
         return factory
 
     def create_client_factory(self, target_id):
         factory = ClientFactory()
-        self.configure_factory(factory)
+        factory.protocol = OutgoingProtocol
+        factory.peer_id = self._peer_service.peer_id
         factory.target_id = target_id
+        factory.add_outgoing_connection = self.add_outgoing_connection
+        factory.remove_outgoing_connection = self.remove_outgoing_connection
+        factory.allow_connection = self.allow_connection
 
         return factory
 
-    def add_connection(self, connection):
-        assert isinstance(connection, PixtreamProtocol)
-        assert hasattr(connection, 'other_id')
-        assert isinstance(connection.other_id, str)
+    def add_incoming_connection(self, connection):
+        assert isinstance(connection, IncomingProtocol)
+        assert isinstance(connection.partner_id, str)
 
-        if connection.other_id in self.active_connections:
-            logging.error('Trying to add an existing connection')
+        if connection.partner_id in self.incoming_connections:
+            logging.error('Adding nonexistent connection')
             connection.drop()
             return
 
-        self.active_connections[connection.other_id] = connection
+        self.incoming_connections[connection.partner_id] = connection
 
-    def remove_connection(self, connection):
-        assert isinstance(connection, PixtreamProtocol)
-        assert hasattr(connection, 'other_id')
-        assert isinstance(connection.other_id, str)
+    def remove_incoming_connection(self, connection):
+        assert isinstance(connection, IncomingProtocol)
+        assert isinstance(connection.partner_id, str)
 
-        self.active_connections.pop(connection.other_id, None)
+        if connection.partner_id not in self.incoming_connections:
+            logging.error('Removing nonexistent connection')
+            return
+
+        del self.incoming_connections[connection.partner_id]
+
+    def add_outgoing_connection(self, connection):
+        assert isinstance(connection, OutgoingProtocol)
+        assert isinstance(connection.partner_id, str)
+
+        if connection.partner_id in self.outgoing_connections:
+            logging.error('Adding nonexistent connection')
+            connection.drop()
+            return
+
+        self.outgoing_connections[connection.partner_id] = connection
+
+    def remove_outgoing_connection(self, connection):
+        assert isinstance(connection, OutgoingProtocol)
+        assert isinstance(connection.partner_id, str)
+
+        if connection.partner_id not in self.outgoing_connections:
+            logging.error('Removing nonexistent connection')
+            return
+
+        del self.outgoing_connections[connection.partner_id]
 
     def allow_connection(self, peer_id):
-        return peer_id not in self.active_connections
+        return peer_id not in self.contacted_peers
 
     def check_connections(self):
-        connections = self.active_connections.keys()
-        logging.debug('Active connections ' + str(connections))
+        incoming_connections = self.incoming_connections.keys()
+        outgoing_connections = self.outgoing_connections.keys()
+        logging.debug('Incoming connections ' + str(incoming_connections))
+        logging.debug('Outgoing connections ' + str(outgoing_connections))
         self._contact_peers()
 
     def listen(self):
@@ -70,15 +104,17 @@ class ConnectionManager(object):
         reactor.listenTCP(self._peer_service.port, factory)
 
     def connect_to_peer(self, peer):
+        assert isinstance(peer, Peer)
+
+        logging.debug('Connecting to peer:' + peer.id)
         factory = self.create_client_factory(peer.id)
         reactor.connectTCP(peer.ip, peer.port, factory)
 
     def _contact_peers(self):
         available_peers = self._peer_service.available_peers
-        contacted_peers = set(self.active_connections.iterkeys())
 
         not_contacted_peers = [peer for peer in available_peers
-                               if peer.id not in contacted_peers]
+                               if peer.id not in self.contacted_peers]
 
         for peer in not_contacted_peers:
             self.connect_to_peer(peer)
--- pixtream/peer/protocol.py
+++ pixtream/peer/protocol.py
@@ -5,34 +5,30 @@ from twisted.protocols.basic import Int32StringReceiver
 from pixtream.peer.messages import HandshakeMessage, Message
 from pixtream.peer.messages import MessageDecodingError
 
-class PixtreamProtocol(Int32StringReceiver):
+class BaseProtocol(Int32StringReceiver):
 
     def __init__(self):
-        self.other_id = None
-        self.handshaked = False
+        self.partner_id = None
+        self.outgoing_handshaked = False
+        self.incoming_handshaked = False
 
     @property
-    def peer_id(self):
-        return self.factory.peer_id
+    def handshaked(self):
+        return self.outgoing_handshaked and self.incoming_handshaked
 
-    def connectionMade(self):
-        logging.debug('Connection made {0}'.format(self.peer_id))
-        if self.other_id is not None:
-            self.send_hanshake()
-
-    def connectionLost(self, reason):
-        logging.debug('Connection lost {0}. Reason: {1}'.format(self.peer_id,
-                                                                reason))
-        self.factory.remove_connection(self)
+    @property
+    def partner_name(self):
+        return str(self.transport.getPeer())
 
     def stringReceived(self, message):
-        logging.debug('Received message: ' + repr(message))
+        logging.debug('Received: {0} from {1}'.format(repr(message),
+                                                      self.partner_name))
 
         try:
             message_object = Message.decode(message)
         except MessageDecodingError as error:
-            logging.error(str(error))
-            self.transport.loseConnection()
+            logging.error('Decoding error ' + str(error))
+            self.drop()
             return
 
         if isinstance(message_object, HandshakeMessage):
@@ -43,38 +39,89 @@ class PixtreamProtocol(Int32StringReceiver):
         self.transport.write(message)
 
     def send_hanshake(self):
-        logging.debug('Sending Handshake')
+        logging.debug('Sending Handshake to ' + self.partner_name)
         msg = HandshakeMessage(self.factory.peer_id)
         self.send_message_object(msg)
+        self.outgoing_handshaked = True
 
-    def receive_handshake(self, msg):
-        logging.debug('Handshake received')
+    def check_incoming_handshake(self, msg):
+        if self.incoming_handshaked:
+            logging.error('Double handshake from id ' + self.partner_id)
+            return False
 
         if not msg.validate():
             logging.error('Wrong handshake. Closing connection')
             self.drop()
-            return
+            return False
 
         if not self.factory.allow_connection(msg.peer_id):
             logging.error('Connection not allowed with ' + msg.peer_id)
             self.drop()
+            return False
+        return True
+
+class IncomingProtocol(BaseProtocol):
+    def connectionMade(self):
+        logging.debug('Incoming connection made: ' + self.partner_name)
+
+    def connectionLost(self, reason):
+        logging.debug('Incoming connection lost: ' + str(reason))
+
+        if self.handshaked:
+            self.factory.remove_incoming_connection(self)
+
+    def receive_handshake(self, msg):
+        logging.debug('Handshake received from ' + self.partner_name)
+
+        if not self.check_incoming_handshake(msg):
             return
 
-        if not self.verify_peer(msg.peer_id):
-            logging.error('Expected a different peer ' + msg.peer_id)
+        logging.debug('Good handshake ' + msg.peer_id)
+
+        self.partner_id = msg.peer_id
+        self.send_hanshake()
+        self.factory.add_incoming_connection(self)
+
+        self.incoming_handshaked = True
+
+    def drop(self):
+        self.transport.loseConnection()
+
+class OutgoingProtocol(BaseProtocol):
+
+    def connectionMade(self):
+        logging.debug('Outgoing connection made ' + self.partner_name)
+        self.send_hanshake()
+
+    def connectionLost(self, reason):
+        logging.debug('Incoming connection lost' + str(reason))
+        if self.handshaked:
+            self.factory.remove_outgoing_connection(self)
+        self.drop()
+
+    def receive_handshake(self, msg):
+        logging.debug('Handshake received from ' + self.partner_name)
+
+        if not self.check_incoming_handshake(msg):
+            return
+
+        if not self.outgoing_handshaked:
+            logging.error('Unrequested handshake from ' + self.partner_name)
+            self.drop()
+            return
+
+        if msg.peer_id != self.factory.target_id:
+            logging.error('Handshake received from unexpected peer')
             self.drop()
             return
 
         logging.debug('Good handshake ' + msg.peer_id)
-        self.other_id = msg.peer_id
-        self.factory.add_connection(self)
-        self.send_hanshake()
-        self.handshaked = True
 
-    def verify_peer(self, peer_id):
-        if self.factory.target__id is not None:
-            return peer_id == self.factory.target_id
+        self.partner_id = msg.peer_id
+        self.factory.add_outgoing_connection(self)
+
+        self.incoming_handshaked = True
 
     def drop(self):
-        self.loseConnection()
+        self.transport.loseConnection()
 
