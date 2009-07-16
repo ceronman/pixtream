454eaa9806dffed176205b45ff0a115cf5ed7c24
Manuel Cerón <ceronman@gmail.com>
2009-07-16 02:30:19 -0500

    Added streamclient module for getting a stream from a traditional streaming server

---
+++ pixtream/peer/streamclient.py
@@ -0,0 +1,43 @@
+'''
+Clients for ordinary streaming systems
+'''
+
+import logging
+
+from twisted.internet.protocol import Protocol, ClientFactory
+from twisted.internet import reactor
+
+from pixtream.peer.splitter import Splitter
+
+class TCPStreamClientProtocol(Protocol):
+
+    def dataReceived(self, data):
+        self.factory.data_received(data)
+
+    def connectionLost(self, reason):
+        self.factory.connection_lost()
+
+class StreamClient(object):
+
+    def __init__(self, splitter):
+        self.splitter = splitter
+
+    def data_received(self, data):
+        self.splitter.feed_stream(data)
+
+    def connection_lost(self):
+        self.splitter.end_stream()
+
+class TCPStreamClient(StreamClient):
+
+    def create_client_factory(self):
+        factory = ClientFactory()
+        factory.protocol = TCPStreamClientProtocol
+        factory.data_received = self.data_received
+        factory.connection_lost = self.connection_lost
+        return factory
+
+    def connect(self, host, port):
+        factory = self.create_client_factory()
+        reactor.connectTCP(host, port, factory)
+
