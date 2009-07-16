ad488ce1faa9c815c920ddb2ac2bbbf6e1653f96
Manuel Cerón <ceronman@gmail.com>
2009-07-16 02:26:53 -0500

    Added splitter.py module. It splits an stream in pixtream packets.

---
+++ pixtream/peer/splitter.py
@@ -0,0 +1,40 @@
+'''
+Splits streaming data into Pixtream protocol packages
+'''
+
+from collections import deque
+
+from pixtream.util.event import Event
+from pixtream.peer.messages import DataPackageMessage
+
+class Splitter(object):
+
+    def __init__(self, packet_size):
+        self.packet_size = packet_size
+        self.on_new_packet = Event()
+        self.on_stream_end = Event()
+
+        self._buffer = bytes()
+        self._packets = deque()
+        self._current_sequence = 0
+
+    def feed_stream(self, data):
+        self._buffer = self._buffer + data
+
+        if len(self._buffer) >= self.packet_size:
+            packet_data = self._buffer[:self.packet_size]
+            self._buffer = self._buffer[self.packet_size:]
+            self._create_packet(packet_data)
+
+    def end_stream(self):
+        self._create_packet(self._buffer)
+        self.on_stream_end.call(self)
+
+    def pop_packet(self):
+        return self._packets.popleft()
+
+    def _create_packet(self, packet_data):
+        packet = DataPackageMessage(self._current_sequence, packet_data)
+        self._packets.append(packet)
+        self._current_sequence += 1
+        self.on_new_packet.call(self)
