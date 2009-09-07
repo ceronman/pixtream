004ce54e7d2408cde9e90a00b525d479cfd5b0b5
Manuel Cerón <ceronman@gmail.com>
2009-09-07 17:26:40 -0500

    Added most_wanted_piece and best_partner_by_peer methods to PieceManager

--- src/pixtream/peer/piecemanager.py
+++ src/pixtream/peer/piecemanager.py
@@ -2,18 +2,54 @@
 Piece manager
 """
 
+import logging
+import itertools
+
 class PieceManager(object):
     def __init__(self):
+        self.last_continuous_piece = 0
         self.own_pieces = set()
         self.pieces_by_partner = {}
+        self.partner_by_piece = {}
 
     def got_new_piece(self, piece):
         self.own_pieces.add(piece)
+        self._update_last()
+        logging.info('Last piece: {0}'.format(self.last_continuous_piece))
 
     def partner_got_piece(self, partner, piece):
-        partner_pieces = self.pieces_by_partner.setdefault(partner, set())
-        partner_pieces.add(piece)
+        pieces = self.pieces_by_partner.setdefault(partner, set())
+        pieces.add(piece)
+
+        partners = self.partner_by_piece.setdefault(piece, set())
+        partners.add(partner)
 
     def partner_got_pieces(self, partner, pieces):
-        partner_pieces = self.pieces_by_partner.setdefault(partner, set())
-        partner_pieces.update(pieces)
+        for piece in pieces:
+            self.partner_got_piece(partner, piece)
+
+    def most_wanted_pieces(self, num):
+        pieces = []
+        for piece in itertools.count(self.last_continuous_piece):
+            if piece not in self.own_pieces:
+                pieces.append(piece)
+            if len(pieces) == num:
+                break
+
+        return pieces
+
+    def best_partner_for_piece(self, piece):
+        partners = self.partner_by_piece.get(piece, [])
+
+        if len(partners) == 0:
+            return None
+
+        else:
+            return next(iter(partners))
+
+    def _update_last(self):
+        for piece in itertools.count(self.last_continuous_piece):
+            if piece in self.own_pieces:
+                self.last_continuous_piece = piece
+            else:
+                break
